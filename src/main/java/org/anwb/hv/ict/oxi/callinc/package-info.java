7750d10ebb86c5608158ea11e8ad6097ac763cce
Chi-Yen Ho <cho@anwb.nl>
2017-01-27 13:44:31 +0100

    still wip

--- src/main/java/org/anwb/hv/ict/oxi/callinc/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://anwb.org/hv/ict/oxi/callinc", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.anwb.hv.ict.oxi.callinc;
